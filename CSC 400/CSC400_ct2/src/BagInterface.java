e45cf7a1ab9a7d7f41a913bac2a9887999643f7c
Greg <gregn913@gmail.com>
2020-09-23 04:26:12 -0600

    CSC400 module 2

---
+++ CSC 400/CSC400_ct2/src/BagInterface.java
@@ -0,0 +1,24 @@
+
+public interface BagInterface
+{
+	public boolean add(Item inItem);
+	public boolean isFull();
+	public Item[] toArray();
+	public int getCurrentSize();
+	public boolean isEmpty();
+	
+	
+	/*	Additional Methods for Reference
+	 * 	
+	 * public int getCurrentSize();
+	 * public boolean remove (Item anEntry);
+	 * public int getFrequencyOf(Item anEntry);
+	 * public boolean contains(Item anEntry);
+	 * public boolean add(T anEntry);
+	 * public T remove;
+	 * public void clear();
+	 * public T[] toArray();
+	 */
+	
+	
+}
